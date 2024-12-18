51f6c53270a3da77bfe98d3c640986b6765503a8
JuanDeucher <juandeucher@outlook.com>
2024-12-17 21:14:08 -0300

    atualização do vantagens

--- Assets/script/vantagens.js
+++ Assets/script/vantagens.js
@@ -1,15 +1,31 @@
-const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show'); // Apenas adiciona a classe "show" ao elemento
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+            entry.target.classList.add('rightOn')
+            entry.target.classList.add('lefttOn')
+            entry.target.classList.add('animation')
         } else {
-            entry.target.classList.remove('show'); // Remove a classe "show" quando sai da tela
+            entry.target.classList.remove('show')
+            entry.target.classList.remove('rightOn')
+            entry.target.classList.remove('lefttOn')
+            entry.target.classList.remove('animation')
         }
-    });
-}, {
-    threshold: 0.5, // 50% do elemento deve estar visível para iniciar a animação
-});
+    })
+})
 
-document.querySelectorAll('.animationPair, .animationOdd').forEach((element) => {
-    myObserver.observe(element);
-});
+const elements = document.querySelectorAll('.animationPair')
+
+elements.forEach((element) => myObserver.observe(element))
+
+let coco = document.querySelectorAll('.animationOdd')
+
+coco.forEach((cocos) => myObserver.observe(cocos))
+
+let direita = document.querySelectorAll('.right')
+
+direita.forEach((direitas) => myObserver.observe(direitas))
+
+let esquerda = document.querySelectorAll('.left')
+
+esquerda.forEach((esquerdas) => myObserver.observe(esquerdas))
